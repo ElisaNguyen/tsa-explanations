f0f149386a06b5696d505b0fde49d0bc3d00bcd8
ElisaNguyen <elisa.nguyen@live.de>
2022-08-10 13:42:06 +0200

    Refactor continuity scripts

--- experiments/adl/continuity_adl.py
+++ experiments/adl/continuity_adl.py
@@ -32,8 +32,8 @@ dataset = load_obj('../../data/adl/dataset_max.pkl')
 Defined as max-sensitivity (maximum change in the attributions when perturbing the input)
 """
 
-A_testset_t = load_obj('../../data/quantitative_test_t_A.pkl')
-B_testset_t = load_obj('../../data/quantitative_test_t_B.pkl')
+A_testset_t = load_obj('../../data/adl/quantitative_test_t_A.pkl')
+B_testset_t = load_obj('../../data/adl/quantitative_test_t_B.pkl')
 A_y_true = dataset['y_test_A'][:, A_testset_t]
 B_y_true = dataset['y_test_B'][:, B_testset_t]
 expl_types = ['s', 'ns', 'sam']
@@ -41,9 +41,9 @@ expl_types = ['s', 'ns', 'sam']
 with torch.no_grad():
     # generate the perturbed data
     X_perturbed_A = generate_perturbed_data(dataset['X_test_A'], A_testset_t,
-                                            '../evaluation/continuity/perturbed_X_A.pkl')
+                                            'continuity/perturbed_X_A.pkl')
     X_perturbed_B = generate_perturbed_data(dataset['X_test_B'], B_testset_t,
-                                            '../evaluation/continuity/perturbed_X_B.pkl')
+                                            'continuity/perturbed_X_B.pkl')
 
     for nb_layer in range(3):
         for expl_type in expl_types:
@@ -51,18 +51,18 @@ with torch.no_grad():
             # A
             perturbed_explanations = compute_perturbed_explanation(nb_layer, X_perturbed_A, dataset['y_test_A'],
                                                                    A_testset_t, expl_type,
-                                                                   '../evaluation/continuity/{}/perturbed_explanations_{}A.pkl'.format(
+                                                                   '.continuity/{}/perturbed_explanations_{}A.pkl'.format(
                                                                        expl_type, nb_layer))
-            model_explanations = load_obj('../evaluation/{}/{}L_explanations_A.pkl'.format(expl_type, nb_layer))
+            model_explanations = load_obj('explanations/{}/{}L_explanations_A.pkl'.format(expl_type, nb_layer))
             max_sensitivity = max_sensitivity_score(A_testset_t, model_explanations, perturbed_explanations)
-            save_obj(max_sensitivity, '../evaluation/continuity/{}/max_sensitivity_{}A.pkl'.format(expl_type, nb_layer))
+            save_obj(max_sensitivity, 'continuity/{}/max_sensitivity_{}A.pkl'.format(expl_type, nb_layer))
             # B
             perturbed_explanations = compute_perturbed_explanation(nb_layer, X_perturbed_B, dataset['y_test_B'],
                                                                    B_testset_t, expl_type,
-                                                                   '../evaluation/continuity/{}/perturbed_explanations_{}B.pkl'.format(
+                                                                   'continuity/{}/perturbed_explanations_{}B.pkl'.format(
                                                                        expl_type, nb_layer
                                                                    ))
-            model_explanations = load_obj('../evaluation/{}/{}L_explanations_B.pkl'.format(expl_type, nb_layer))
+            model_explanations = load_obj('explanations/{}/{}L_explanations_B.pkl'.format(expl_type, nb_layer))
             max_sensitivity = max_sensitivity_score(B_testset_t, model_explanations, perturbed_explanations)
-            save_obj(max_sensitivity, '../evaluation/continuity/{}/max_sensitivity_{}B.pkl'.format(expl_type, nb_layer))
+            save_obj(max_sensitivity, 'continuity/{}/max_sensitivity_{}B.pkl'.format(expl_type, nb_layer))
         print('Continuity evaluation of SNN-{}L is done!'.format(nb_layer))
